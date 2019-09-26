9f2ae72f3455c4306b60c3705bd496fd90f0aa0a
Aliskhan <aliskhangazamatov@gmail.com>
2019-09-27 00:15:17 +0300

    build 27.09
    test

--- dist/js/main.js
+++ dist/js/main.js
@@ -20,7 +20,8 @@ $(document).ready(function(){
         margin: 60,
         responsive:{
             0:{
-                items:1
+                items:1,
+
             },
             600:{
                 items:5
@@ -131,5 +132,48 @@ $(document).ready(function(){
         accordionToggles[i].addEventListener('click', switchAccordion, false);
     }
 })();
+
+var GM = {
+    init: function () {
+        this.initCache();
+        this.initMap();
+    },
+
+    initCache: function() {
+        this.$popupContent = $('.js-marker-content');
+    },
+
+    initMap: function () {
+        var coordinates = {lat: 47.106561, lng: 51.888614},
+            popupContent = this.$popupContent.html(),
+            markerImage = './img/marker2.png',
+            zoom = 15,
+
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: coordinates,
+                zoom: zoom,
+                disableDefaultUI: true,
+                scrollwheel: false
+            }),
+
+            infowindow = new google.maps.InfoWindow({
+                content: popupContent
+            }),
+
+            marker = new google.maps.Marker({
+                position: coordinates,
+                map: map,
+                icon: markerImage
+            });
+
+
+    }
+};
+
+$(document).ready(function() {
+    GM.init();
+});
+
+
 
 
